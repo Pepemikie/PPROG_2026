e0253e0d84fa9614e5224dca4213eec7bd69a540
José Miguel Romero Oubiña <josemi.instituto.gtb@gmail.com>
2026-02-24 12:01:36 +0100

    Character-Documentado

--- character.c
+++ character.c
@@ -1,49 +1,64 @@
+/**
+ * @brief It implements the character module
+ *
+ * @file character.c
+ * @author Jorge Garcia Garrido 
+ * @version 0
+ * @date 23-02-2026
+ * @copyright GNU Public License
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "character.h"
 
+/*Defined Character struct*/
 struct _Character {
-  Id id;
-  char name[WORD_SIZE];
-  char gdesc[GDESC_SIZE]; /* Cadena de 6 caracteres + \0 */
-  int health;             /* Puntos de vida */
-  Bool friendly;          /* ¿Es amigo? */
-  char message[WORD_SIZE];/* Mensaje que almacena */
+  Id id; /*The ID of the character*/
+  char name[WORD_SIZE];/*The name given to the character*/
+  char gdesc[GDESC_SIZE]; /*String of 6 characters + \0*/
+  int health;             /* Life points*/
+  Bool friendly;          /*Defines if the character is your friend or not*/
+  char message[WORD_SIZE];/*Storage the message*/
 };
 
+/*Initialises everything by default and allocates memory for a character*/
 Character* character_create(Id id) {
   Character *c = NULL;
-  if (id == NO_ID) return NULL;
+  if (id == NO_ID) return NULL;/*Error control*/
 
-  c = (Character*) malloc(sizeof(Character));
-  if (!c) return NULL;
+  c = (Character*) malloc(sizeof(Character));/*Allocates memory for the character*/
+  if (!c) return NULL;/*Error control*/
 
-  /* Inicialización por defecto */
+  /*Initialises the character Id, the description, the life points, the friendly mood and the message by default*/
   c->id = id;
   strcpy(c->name, "");
-  strcpy(c->gdesc, "      "); /* 6 espacios */
-  c->health = 10;             /* Vida inicial por defecto */
+  strcpy(c->gdesc, "      ");
+  c->health = 10;
   c->friendly = TRUE;
   strcpy(c->message, "");
 
-  return c;
+  return c; /*returns the pointer of the struct with all initilizated*/
 }
 
+/*Frees the character information*/
 Status character_destroy(Character* c) {
-  if (!c) return ERROR;
-  free(c);
+  if (!c) return ERROR;/*Checks if the character exists*/
+  free(c);/*frees the character*/
   return OK;
 }
 
-/* Ejemplo de implementación de Getter/Setter */
+/*Example of the implementation of Getter/Setter*/
+
+/*Checks if the character exists and retur its health*/
 int character_get_health(Character* c) {
-  return (c) ? c->health : -1;
+  return (c) ? c->health : -1;/*Checks if Character exists, if it is so, returns c->health; otherwise, return -1*/
 }
 
+/*Sets the characters health to the health passed by arguments*/
 Status character_set_health(Character* c, int health) {
-  if (!c || health < 0) return ERROR;
-  c->health = health;
+  if (!c || health < 0) return ERROR;/*Error control*/
+  c->health = health;/*Gives the life points passed by argyment to the character's health */
   return OK;
 }
 
